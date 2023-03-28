ca6f6ab49be1fa1a16d56d1c8bc258eb3eb1f700
Chevyjd2007 <112202840+Chevyjd2007@users.noreply.github.com>
2023-03-27 22:47:46 -0400

    jh

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import Home from './components/home/Home';
+import { Home } from './components/home/Home';
 
 function App() {
   return (
-    <main className='main'>
+    <main className="main">
       <Home />
     </main>
   );
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import galaxyshape from '../../assets/galaxy-shape-1.png';
 import zeldafairies from "../../assets/zelda-fairies-1.png";
 
-import {BsGithub, AiOutlineMail} from 'react-icons/fa';
+//import {BsGithub} from 'react-icons/fa';
+
 import './home.css';
 
-const Home = () => {
+export const Home = () => {
   return (
     <section className='home' id='home'>
-      <div className='home__container container'>
-       <p className='home__subtitle text-cs'>
+    <div className='home__container container'>
+      <p className='home__subtitle text-cs'>
         What's up? <span>I am</span>
-       </p>
+      </p>
 
-      <h1 className='home__title text-cs'>
-        <span>Josias</span> Chevalier
-      </h1>
+      
+  <h1 className='home__title text-cs'>
+    <span>Josias</span> Chevalier
+  </h1>
 
-      <p className='home__job'>
+  <p className='home__job'>
         <span className='text-cs'>I'm an aspiring</span> <b>full stack Software Engineer</b>
       </p>
 
@@ -26,26 +28,15 @@ const Home = () => {
         at Florida International University. 
       </p>
 
-      <div className='home__socials'>
-        <a href='' className='home__social-link'>
-            <BsGithub /> 
-        </a>
-        <div className='home__socials'>
-        <a href='' className='home__social-link'>
-            <AiOutlineMail /> 
-        </a>
-        
-      </div>
-
       <div className="home__btns">
             <a href="" className="btn">Download CV</a>
 
             <a href="" className="hero__link">My Skills</a>
-      </div>
-      </div>
-      </div>
-    </section>
-  )
+      
+</div>
+</div>
+</section>
+  );
 };
 
 export default Home;
